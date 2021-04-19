5066280add27ba3c7c634e14737fbfd69dfd71e6
SoddenFilly <aidanmachue@gmail.com>
2021-04-19 12:14:40 +1200

    .

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ server.get('/', (req,res) => {
     res.send("Shalome Plebber")
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log("bobayah")
 })
