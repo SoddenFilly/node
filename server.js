b41035361f2c3f8ea5b98890171e8ffc0a773ce3
SoddenFilly <82764925+SoddenFilly@users.noreply.github.com>
2021-04-19 12:36:20 +1200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express();
 
 server.get('/', (req,res) => {
-    res.send("Homeppage")
+    res.send("Homepage")
 })
 
 const port = process.env.port || 3000;
