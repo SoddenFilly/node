368317e05f7668ae0b9dda05d0ef814dcd6df011
SoddenFilly <aidanmachue@gmail.com>
2021-04-19 12:35:45 +1200

    .

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express();
 
 server.get('/', (req,res) => {
-    res.send("Homepage")
+    res.send("Homepage Update")
 })
 
 const port = process.env.port || 3000;
