ccab69abd749c31dac406a33043d4f5b2be34c97
SoddenFilly <aidanmachue@gmail.com>
2021-04-19 13:41:00 +1200

    .

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ server.get('/bb', (req,res) => {
     //res.sendFile(path.join(__dirname + '/style.css'));
     res.send("Homepage Update bb")
 })
+server.all('/secret', function (req, res, next) {
+    console.log('Accessing the secret section ...')
+    next() // pass control to the next handler
+})
 
 const port = process.env.port || 3000;
 server.listen(port, () => {
