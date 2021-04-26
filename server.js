38362a133e36642662ffb4002174442c4fdd59f6
SoddenFilly <aidanmachue@gmail.com>
2021-04-26 20:54:09 +1200

    .

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const express = require('express')
 const server = express();
 const path = require('path')
+const port = process.env.port || 3000;
 
 // server.use('/static', express.static(path.join(__dirname, 'public')))
 server.use(express.static(__dirname+'/public'));
 
-server.get('/', (req,res) => {
+server.get('/', function (req,res) {
     //res.sendFile(path.join(__dirname + '/index.html'));
     res.send("Homepage Update")
 })
-server.get('/login', (req,res) => {
+server.get('/login', function (req,res) {
     res.sendFile('./public/login.html', { root: __dirname });
 })
 server.get('/game', function (req,res) {
     // res.sendFile(path.join(__dirname + '/public/game.html'));
     res.sendFile('./public/game.html', { root: __dirname });
-})
-
-const port = process.env.port || 3000;
-server.listen(port, () => {
-    console.log("Bobayah")
 })
