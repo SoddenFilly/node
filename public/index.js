2e5189f563c73b254ec4d3f05323d93ca43a9031
SoddenFilly <aidanmachue@gmail.com>
2021-05-02 18:34:56 +1200

    Aidan

--- public/index.js
+++ public/index.js
@@ -2,9 +2,37 @@ const root_url = "http://node-env.eba-gnikufyj.ap-southeast-2.elasticbeanstalk.c
 function buttons(spes) {
     window.location.href = root_url + spes
 }
+var fade_active = "True"
 function fade() {
-    var x = document.querySelectorAll('.fade,.fade-out');
-    for (let i = 0; i < x.length; i++) { 
-        x[i].className += '-out';
+    if (fade_active === "True"){
+        document.querySelectorAll('.veil')[0].className += "-in"
+        var x = document.querySelectorAll('.fade,.fade-out');
+        for (let i = 0; i < x.length; i++) { 
+            x[i].className += '-out';
+        }
+        setTimeout(function(){
+            for (let i = 0; i < document.querySelectorAll('.fade-out').length; i++) { 
+                document.querySelectorAll('.fade-out')[i].style.display = 'none';
+            }
+            document.querySelectorAll('.veil-in')[0].className = "veil"
+            var x = document.querySelectorAll('.fade,.fade-out');
+            for (let i = 0; i < x.length; i++) { 
+                x[i].style.display = 'block';
+                x[i].className = 'fade-in';
+            }
+            reloadCss()
+        }, 2000)
+    // fade_active = "False"
+    } else {
+        document.querySelectorAll('.veil-in')[0].className = "veil"
+    }
+}
+function reloadCss(){
+    var links = document.getElementsByTagName("link");
+    for (var cl in links)
+    {
+        var link = links[cl];
+        if (link.rel === "stylesheet")
+            link.href += "";
     }
 }
