ef42a026346faec4842aee78ffb49b8233193c69
SoddenFilly <aidanmachue@gmail.com>
2021-05-23 00:45:53 +1200

    Aidan-_'

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = express();
 const path = require('path')
 const port = process.env.port || 3000;
 
-server.use(express.static(__dirname+'/public'));
+server.use(express.static(__dirname+'/public')); 
 
 server.get('/', (req,res) => {
     res.sendFile('./public/index.html', { root: __dirname });
