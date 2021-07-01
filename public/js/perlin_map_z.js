4933f361bfe611e952d0a94beeeb2a8ba675d818
Echoing-Thunder <fakenewsknight@gmail.com>
2021-07-01 01:23:16 -0700

    Zach-'

--- public/js/perlin_map_z.js
+++ public/js/perlin_map_z.js
@@ -10,59 +10,67 @@ function go(RES){
     // const SIZE = 300;
     const SIZE = 300;
     var HIG = WID = SIZE;
-  
+ 
     var count = 0;
     for (let y = 0; y < RES; y += RES / HIG){
-        
+       
         for (var x = 0; x < RES; x += RES / WID){
             let color_val = parseInt(perlin.get(x, y) * 255);
             count ++;
 
-            let veryHigh = 80;
-            let high = 20;
-            let medium = -80;
-            let low = -80;
-
             // if (x >= RES*0.99){
             //     arr = arr + [1]
             // }
-        
-            if (color_val >= veryHigh){
-                // arr = arr + [1]
-                arr[arr.length] = 5
+
+            function testing(num1, num2, length) {
+                if (color_val >= num1 && color_val < num2) {
+                    arr[arr.length] = length
+                }
             }
-            else if (color_val >= high && color_val <= veryHigh){
-                // arr = arr + [2]
-                arr[arr.length] = 4
-            }
-            else if (color_val >= medium && color_val <= high){
-                // arr = arr + [3]
-                arr[arr.length] = 3
-            }
-            else if (color_val >= low && color_val <= medium){
-                // arr = arr + [4]
-                arr[arr.length] = 2
-            }
-            else if (color_val <= low){
-                // arr = arr + [5]
-                arr[arr.length] = 1
-            }
-            
-        }
-        // console.log(x)
+
+            //block 4
+            testing(144, 300, 20)
+            testing(128, 144, 19)
+            testing(112, 128, 18)
+            testing(96, 112, 17)
+            testing(80, 96, 16)
+
+            //block 3
+            testing(68, 80, 15)
+            testing(56, 68, 14)
+            testing(44, 56, 13)
+            testing(32, 44, 12)
+            testing(20, 32, 11)
+
+            //block 2
+            testing(0, 20, 10)
+            testing(-20, 0, 9)
+            testing(-40, -20, 8)
+            testing(-60, -40, 7)
+            testing(-80, -60, 6)
+
+            //block 1
+            testing(-96, -80, 5)
+            testing(-112, -96, 4)
+            testing(-128, -112, 3)
+            testing(-144, -128, 2)
+            testing(-300, -144, 1)
+
+
+            // console.log(x)
+            // console.log(count)
+            // document.getElementById("text_container").innerHTML = arr;
+
+            // for (let i = 0; i < arr.length; i++) {
+            //     // console.log(i);
+            //     let temp = arr[i];
+            //     arr[i] = [[temp, 27]];
+            // }
+            // console.log("qq")
+            // console.log(arr)
+            // console.log(arr.length)
+            // console.log("fffff")
+        }    
     }
-    // console.log(count)
-    // document.getElementById("text_container").innerHTML = arr;
-
-    // for (let i = 0; i < arr.length; i++) {
-    //     // console.log(i);
-    //     let temp = arr[i];
-    //     arr[i] = [[temp, 27]];
-    // }
-    // console.log("qq")
-    // console.log(arr)
-    // console.log(arr.length)
-    // console.log("fffff")
-
     return arr
 }// func go
