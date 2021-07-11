fb5f0be672bbb93345a80b5c1d237e6a41d96f81
SoddenFilly <aidanmachue@gmail.com>
2021-07-11 16:14:02 +1200

    Aidan-_'

--- public/js/proc.js
+++ public/js/proc.js
@@ -1,12 +1,59 @@
 console.log("proc.js")
 
-var base = [0,0,0,0]
+var proc = []
+var prc = []
 var arr = []
-for (let i = 0; i < base.length; i++){
-    if (base[i] == 0){
-        arr.push(0)
-    }
-    // arr.push()
-}
+console.log(" arr", arr)
 
-console.log(arr)
+for (let loop = 0; loop < 3; loop++){
+    proc = []
+
+    for (let li = 0; li < loop; li++){ proc.push(1) }
+    for (let li = 0; li < 6 - loop; li++){ proc.push(0) }
+
+    for (let i = 0; i < 6; i++){
+        console.log("proc", proc)
+        console.log(" arr", arr)
+        arr.push(proc)
+        console.log(" arr", arr)
+        if (proc[i] == 0){
+            // arr.push(0)
+        }
+        else if (proc[i] == 1){
+            // arr.push(1)
+            if (proc[i+1] == 0){
+                proc[i+1] = 1
+                proc[i] = 0
+            }
+            else if (proc[i+1] == 1){
+                proc[i+2] = 1
+                proc[i+1] = 0
+                i += 1
+            }
+            if (i == proc.length-1 && loop > 1){
+                // console.log("---")
+                for (let loo = proc.length-2; loo >= 0; loo--){
+                    // console.log(loo)
+                    if (proc[loo] == 1){
+                        proc[loo+1] = 1
+                        proc[loo] = 0
+
+                        proc[loo+2] = 1
+                        if (loo == proc.length-3){
+                            proc[i] = 1
+                        }
+                        else{
+                            proc[i] = 0
+                        }
+                        
+                        // console.log("   2",proc)
+
+                        i = 0
+                        break
+                    }
+                    // console.log("   2",proc)
+                }
+            }
+        }
+    }
+}
