b6d78d73804f47cb43fe3b2db27540a5e34952c1
SoddenFilly <aidanmachue@gmail.com>
2021-07-21 03:52:57 +1200

    Aidan-_'

--- alt_public/js/tilesetmaker.js
+++ alt_public/js/tilesetmaker.js
@@ -13,11 +13,40 @@ function loop() {
     
     // loops the func every half second
     let secs = 0.1 //0.1
-    setTimeout(() => { window.requestAnimationFrame(loop); }, secs*1000);
+    // setTimeout(() => { window.requestAnimationFrame(loop); }, secs*1000);
+    // ctx.beginPath()
+    // ctx.rect(0, 0, 160, height)
+    // ctx.fillStyle = "#9d978b";
+    // ctx.fill();
+    // ctx.beginPath()
+    // ctx.rect(23, 48, 112, height)
+    // ctx.fillStyle = "#8c877c";
+    // ctx.fill();
 
+    // ctx.beginPath()
+    // for (let y = 0; y < 20; y++){
+        
+    //     for (let x = 0; x < 5; x++){
+    //         ctx.rect(x*22 + 25, y*22 + 50, 20, 20)
+    //         ctx.fillStyle = "#444444";
+    //     }
+    // }
+    // ctx.fill();
 
-
+    for (let y = 0; y < 20; y++){
+        
+        for (let x = 0; x < 5; x++){
+            var tile = document.createElement('div');
+            tile.id = "tile"
+            // tile = document.getElementById("tile")
+            tile.style.width = "10px"
+            tile.style.height = "10px"
+            tile.style.backgroundColor = "#ffffff"
+            document.body.appendChild(tile);
+        }
+    }
 }
+
 
 //#endregion Functions end
 
@@ -46,6 +75,8 @@ tile_min.src = "../resources/images/min_20.png";
 //#endregion Set up end
 
 //#region Procedural
+
+
 
 tile_min.addEventListener("load", () => { loop(); });
 
